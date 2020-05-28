c180e8dd73f4a2e3a7b402c1805f8891c2a46bc8
Gergely Pap <gergely.tibor.pap.js@gmail.com>
2020-05-28 13:04:29 +0200

    initial commit

---
+++ axios-error-converter.js
@@ -0,0 +1,21 @@
+class ConvertedAxiosError extends Error {
+
+  constructor (axiosError) {
+    const response = axiosError.response && {
+      data: axiosError.response.data,
+      status: axiosError.response.status,
+      headers: axiosError.response.headers
+    };
+    const errorMessage = response && response.data && (response.data.message || axiosError.message);
+    const statusCode = response && response.status;
+
+    super(errorMessage);
+    this.name = this.constructor.name;
+
+    this.config = axiosError.config;
+    this.statusCode = statusCode;
+    this.response = response;
+  }
+}
+
+module.exports = ConvertedAxiosError;
