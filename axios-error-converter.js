ff0048e4884fbf5ea301f9a5109bdf9f47e4575c
Gergely Pap <gergely.tibor.pap.js@gmail.com>
2020-05-28 14:22:49 +0200

    fix error message

--- axios-error-converter.js
+++ axios-error-converter.js
@@ -1,17 +1,17 @@
 class ConvertedAxiosError extends Error {
 
-  constructor (axiosError) {
+  constructor(axiosError) {
     const response = axiosError.response && {
       data: axiosError.response.data,
       status: axiosError.response.status,
-      headers: axiosError.response.headers
+      headers: axiosError.response.headers,
     };
-    const errorMessage = response && response.data && (response.data.message || axiosError.message);
+    const errorMessage = response && response.data && response.data.message || axiosError.message;
     const statusCode = response && response.status;
-
+    
     super(errorMessage);
     this.name = this.constructor.name;
-
+    
     this.config = axiosError.config;
     this.statusCode = statusCode;
     this.response = response;
